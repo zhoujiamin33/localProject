2fbb16c2693a912b990137e96f8d15aee8dc7724
WangJiaHao <1486964584@qq.com>
2021-06-16 21:14:50 +0800

    批量审批修改招生状态

--- ThirdProject/thirdproject/src/main/java/com/trkj/thirdproject/service/MemorandumattachmentServiceImpl.java
+++ ThirdProject/thirdproject/src/main/java/com/trkj/thirdproject/service/MemorandumattachmentServiceImpl.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.Date;
 import java.util.List;
 
 @Slf4j
@@ -13,8 +14,18 @@ import java.util.List;
 public class MemorandumattachmentServiceImpl implements MemorandumattachmentService {
     @Resource
     private MemorandumattachmentDao memorandumattachmentDao;
+    //  显示
     @Override
     public List<Memorandumattachment> selectMemorandumAttachment() {
         return memorandumattachmentDao.selectMemorandumAttachment();
     }
+    // 批量审批修改招生状态
+    @Override
+    public int PiliangZssp(List<Integer> MemorandumattachmentId, Date JWExamineTime){
+        for (int unitid :MemorandumattachmentId){
+            memorandumattachmentDao.PiliangZssp(unitid,JWExamineTime);
+        }
+        return 1;
+    }
+
 }
